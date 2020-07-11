978fe68b6b28efe6a45491608004d70a9814f2a6
Jakub <jswierczek213@gmail.com>
2020-07-11 12:51:53 +0200

    navbars added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio';
+  constructor() {}
+
+  @ViewChild('aboutMe', { static: true }) aboutMe: ElementRef;
+  @ViewChild('skills', { static: true }) skills: ElementRef;
+  @ViewChild('projects', { static: true }) projects: ElementRef;
+  @ViewChild('contact', { static: true }) contact: ElementRef;
+
+  scrollToSection(sectionNumber: number) {
+    if (sectionNumber === 0) {
+      const el = this.aboutMe.nativeElement;
+      el.scrollIntoView({ behavior: 'smooth'});
+    }
+
+    if (sectionNumber === 1) {
+      const el = this.skills.nativeElement;
+      el.scrollIntoView({ behavior: 'smooth'});
+    }
+
+    if (sectionNumber === 2) {
+      const el = this.projects.nativeElement;
+      el.scrollIntoView({ behavior: 'smooth'});
+    }
+
+    if (sectionNumber === 3) {
+      const el = this.contact.nativeElement;
+      el.scrollIntoView({ behavior: 'smooth'});
+    }
+  }
 }
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit, HostListener, Output, EventEmitter } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('openCloseNavbar', [
+      state('open', style({
+        left: '0px'
+      })),
+      state('closed', style({
+        left: '-150px'
+      })),
+      transition('open <=> closed', [
+        animate('400ms linear')
+      ])
+    ]),
+    trigger('click', [
+      state('open', style({
+        left: '150px'
+      })),
+      state('closed', style({
+        left: '0px'
+      })),
+      transition('open <=> closed', [
+        animate('400ms linear')
+      ])
+    ])
+  ],
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+  @Output() scrolled = new EventEmitter();
+
+  links = [
+    { name: 'O mnie', icon: 'account_box', active: false },
+    { name: 'Umiejętności', icon: 'assignment', active: false },
+    { name: 'Projekty', icon: 'folder', active: false },
+    { name: 'Kontakt', icon: 'mail', active: false }
+  ];
+  innerWidth: any;
+
+  displayDesktopNavbar: boolean;
+
+  ngOnInit() {
+    this.links[0].active = true;
+    this.displayDesktopNavbar = false;
+
+    this.innerWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+  }
+
+  toggleDesktopNavbar() {
+    this.displayDesktopNavbar = !this.displayDesktopNavbar;
+  }
+
+  viewSection(sectionNumber: number) {
+    // Set active link
+    this.links.forEach((link, index) => {
+      link.active = (index === sectionNumber) ? true : false;
+    });
+
+    // Scroll to section
+    this.scrolled.emit(sectionNumber);
+  }
+
+}
